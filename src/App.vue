6e184c643c5b92a93d4d0c59b029a7b792785026
GyuSanity <slipperyeely@gmail.com>
2024-10-27 16:06:55 +0900

    fix conflict

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-<<<<<<< HEAD
+  <<<<<<< HEAD
   <TodoHeader></TodoHeader>
   <TodoInput @InputUpdate="addTodoItem"></TodoInput>
   <TodoList :todoItems="todoItems"></TodoList>
@@ -36,30 +36,3 @@ todoItems.value = fetchTodos();
 </script>
 
 <style></style>
-=======
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
->>>>>>> f84254d (delete README)
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  server: {
-    port: 8000,
-  },
   transpileDependencies: true,
 });
-=======
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
->>>>>>> f84254d (delete README)
