369d2e41a3ae98000f521f99743a45771f811c14
GyuSanity <slipperyeely@gmail.com>
2024-10-27 16:38:43 +0900

    update

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
   https://joshua1988.github.io/vue-camp/composition/event-emit.html#%E1%84%8F%E1%85%A5%E1%86%B7%E1%84%91%E1%85%A9%E1%84%82%E1%85%A5%E1%86%AB%E1%84%90%E1%85%B3-%E1%84%8B%E1%85%B5%E1%84%87%E1%85%A6%E1%86%AB%E1%84%90%E1%85%B3-%E1%84%87%E1%85%A1%E1%86%AF%E1%84%89%E1%85%A2%E1%86%BC
   -->
   <TodoHeader></TodoHeader>
-  <TodoInput @InputUpdate="addTodoItem"></TodoInput>
+  <!-- <TodoInput @하위컴포넌트이벤트이름="상위컴포넌트의 메서드 이름"></TodoInput> -->
   <TodoInput @InputUpdate="addTodoItem"></TodoInput>
   <!-- <TodoList :프롭스이름="상위 컴포넌트의 데이터 이름"></TodoList> -->
   <TodoList :todoItems="todoItems"></TodoList>
